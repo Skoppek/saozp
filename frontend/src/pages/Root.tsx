28b3c2f7c3fa96c1c9495646dfd2c416672d8712
Skoppek <skopekv21@gmail.com>
2024-04-21 22:14:33 +0200

    navbar

--- frontend/src/pages/Root.tsx
+++ frontend/src/pages/Root.tsx
@@ -1,5 +1,15 @@
-import { Button, DarkThemeToggle } from "flowbite-react";
+import { Button, DarkThemeToggle, Navbar } from "flowbite-react";
 
 export const Root = () => {
-  return <DarkThemeToggle />;
+  return (
+    <Navbar fluid className="bg-sky-600 dark:bg-slate-700">
+      <Navbar.Collapse>
+        <Navbar.Link href="#">About</Navbar.Link>
+        <Navbar.Link href="#">Services</Navbar.Link>
+        <Navbar.Link href="#">Pricing</Navbar.Link>
+        <Navbar.Link href="#">Contact</Navbar.Link>
+      </Navbar.Collapse>
+      <DarkThemeToggle />
+    </Navbar>
+  );
 };

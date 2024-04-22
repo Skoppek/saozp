ff7cc4c3f91eda9276f4b36963a75def62644b53
Skoppek <skopekv21@gmail.com>
2024-04-22 22:54:12 +0200

    navbar

--- frontend/src/pages/Root.tsx
+++ frontend/src/pages/Root.tsx
@@ -1,15 +1,23 @@
-import { DarkThemeToggle, Navbar } from "flowbite-react";
+import { Button, DarkThemeToggle, Navbar } from "flowbite-react";
 import { Link, Outlet } from "react-router-dom";
 
 export const Root = () => {
   return (
     <>
       <Navbar fluid className="bg-sky-600 dark:bg-slate-700">
+        <Navbar.Brand as={Link} href="https://flowbite-react.com">
+          <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+            SAOZP
+          </span>
+        </Navbar.Brand>
         <Navbar.Collapse>
           <Link to={"/problems"}>Zadania</Link>
           <Link to={"/submissions"}>Moje rozwiązania</Link>
         </Navbar.Collapse>
-        <DarkThemeToggle />
+        <Navbar.Collapse>
+          <Button>Login</Button>
+          <DarkThemeToggle />
+        </Navbar.Collapse>
       </Navbar>
       <Outlet />
     </>

6a2562338b6149d3a7e378c8acf8b198334c7d65
Skoppek <skopekv21@gmail.com>
2024-05-09 23:50:29 +0200

    eee

--- frontend/src/pages/Root.tsx
+++ frontend/src/pages/Root.tsx
@@ -1,4 +1,4 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import { Navigation } from "../components/Navigation";
 import { createContext, useCallback, useEffect, useState } from "react";
 import apiClient from "../apiClient";
@@ -13,6 +13,7 @@ export const AuthContext = createContext<
 
 export const Root = () => {
   const [isLogged, setIsLogged] = useState(false);
+  const url = useLocation();
 
   const setLogged = useCallback((value: boolean) => {
     setIsLogged(value);
@@ -28,6 +29,14 @@ export const Root = () => {
     <>
       <AuthContext.Provider value={{ isLogged, setIsLogged: setLogged }}>
         <Navigation />
+        {url.pathname === "/" && (
+          <div className="mt-[20vh] flex flex-col items-center">
+            <div className="font-sans text-9xl dark:text-zinc-50/80">SAOZP</div>
+            <div className="text-2xl dark:text-zinc-50/30">
+              System Automatycznej Oceny Zadań Programistycznych
+            </div>
+          </div>
+        )}
         <div className="text-black dark:text-slate-50">
           <Outlet />
         </div>

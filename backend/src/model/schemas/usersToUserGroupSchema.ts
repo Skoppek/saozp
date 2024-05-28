c64f645836e5a4ded50f55e4168bb36ebd2a6c13
Skoppek <skopekv21@gmail.com>
2024-05-28 15:39:57 +0200

    connected users to groups

---
+++ backend/src/model/schemas/usersToUserGroupSchema.ts
@@ -0,0 +1,13 @@
+import { integer } from 'drizzle-orm/pg-core/columns';
+import { pgTable } from 'drizzle-orm/pg-core/table';
+import { userSchema } from './userSchema';
+import { userGroupSchema } from './userGroupSchema';
+
+export const usersToUserGroupSchema = pgTable('users_to_user_groups', {
+    userId: integer('user_id')
+        .notNull()
+        .references(() => userSchema.id),
+    groupId: integer('group_id')
+        .notNull()
+        .references(() => userGroupSchema.id),
+});

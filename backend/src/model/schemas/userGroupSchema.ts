f3ac469686a3888f872120b43d699cf30db7faf3
Skoppek <skopekv21@gmail.com>
2024-05-28 15:34:34 +0200

    created userGroupSchema

---
+++ backend/src/model/schemas/userGroupSchema.ts
@@ -0,0 +1,14 @@
+import { pgTable } from 'drizzle-orm/pg-core/table';
+import { integer, serial, varchar } from 'drizzle-orm/pg-core/columns';
+import { userSchema } from './userSchema';
+
+export const userGroupSchema = pgTable('user_groups', {
+    id: serial('id').primaryKey(),
+    name: varchar('name', { length: 128 }).notNull(),
+    owner: integer('creator_id')
+        .references(() => userSchema.id)
+        .notNull(),
+});
+
+export type NewUserGroup = typeof userGroupSchema.$inferInsert;
+export type UserGroup = typeof userGroupSchema.$inferSelect;

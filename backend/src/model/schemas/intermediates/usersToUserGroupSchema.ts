8c5aed3f39927afcf734d75f9d9afda0de45224b
Skoppek <skopekv21@gmail.com>
2024-05-28 15:41:42 +0200

    created catalog for intermediate tables

--- backend/src/model/schemas/intermediates/usersToUserGroupSchema.ts
+++ backend/src/model/schemas/intermediates/usersToUserGroupSchema.ts
@@ -1,7 +1,7 @@
 import { integer } from 'drizzle-orm/pg-core/columns';
 import { pgTable } from 'drizzle-orm/pg-core/table';
-import { userSchema } from './userSchema';
-import { userGroupSchema } from './userGroupSchema';
+import { userSchema } from '../userSchema';
+import { userGroupSchema } from '../userGroupSchema';
 
 export const usersToUserGroupSchema = pgTable('users_to_user_groups', {
     userId: integer('user_id')

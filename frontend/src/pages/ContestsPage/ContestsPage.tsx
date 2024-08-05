8c1a5ab52abdc1c8269dfb6e3dae7c38c45a78a3
Mateusz Skop <skopekv21@gmail.com>
2024-08-05 20:33:55 +0200

    usuwanie

--- frontend/src/pages/ContestsPage/ContestsPage.tsx
+++ frontend/src/pages/ContestsPage/ContestsPage.tsx
@@ -2,9 +2,9 @@ import { useQuery } from "@tanstack/react-query";
 import { AuthenticatedPage } from "../AuthenticatedPage.tsx";
 import apiClient from "../../client/apiClient.ts";
 import { Table } from "flowbite-react/components/Table";
-import { Button, Spinner } from "flowbite-react";
+import { Button, ListGroup, Popover, Spinner } from "flowbite-react";
 import moment from "moment";
-import { HiDotsVertical } from "react-icons/hi";
+import { HiDotsVertical, HiOutlineTrash } from "react-icons/hi";
 import { useState } from "react";
 import { ContestCreateModal } from "./ContestCreateModal.tsx";
 
@@ -35,36 +35,58 @@ export const ContestsPage = () => {
             >
               Utwórz zawody
             </Button>
-            <Table>
-              <Table.Head>
-                <Table.HeadCell>Nazwa</Table.HeadCell>
-                <Table.HeadCell>Start</Table.HeadCell>
-                <Table.HeadCell>Koniec</Table.HeadCell>
-                <Table.HeadCell></Table.HeadCell>
-              </Table.Head>
-              {!isFetching && data ? (
-                <Table.Body>
-                  {data.map((contest) => (
-                    <Table.Row className="w-full bg-white dark:border-gray-700 dark:bg-gray-800">
-                      <Table.Cell>{contest.name}</Table.Cell>
-                      <Table.Cell>
-                        {moment(contest.startDate).toDate().toLocaleString()}
-                      </Table.Cell>
-                      <Table.Cell>
-                        {moment(contest.endDate).toDate().toLocaleString()}
-                      </Table.Cell>
-                      <Table.Cell>
-                        <Button outline color="gray">
-                          <HiDotsVertical />
-                        </Button>
-                      </Table.Cell>
-                    </Table.Row>
-                  ))}
-                </Table.Body>
-              ) : (
-                <Spinner />
-              )}
-            </Table>
+            <div className="h-screen">
+              <Table className="h-full">
+                <Table.Head>
+                  <Table.HeadCell>Nazwa</Table.HeadCell>
+                  <Table.HeadCell>Start</Table.HeadCell>
+                  <Table.HeadCell>Koniec</Table.HeadCell>
+                  <Table.HeadCell></Table.HeadCell>
+                </Table.Head>
+                {!isFetching && data ? (
+                  <Table.Body>
+                    {data.map((contest) => (
+                      <Table.Row className="w-full bg-white dark:border-gray-700 dark:bg-gray-800">
+                        <Table.Cell>{contest.name}</Table.Cell>
+                        <Table.Cell>
+                          {moment(contest.startDate).toDate().toLocaleString()}
+                        </Table.Cell>
+                        <Table.Cell>
+                          {moment(contest.endDate).toDate().toLocaleString()}
+                        </Table.Cell>
+                        <Table.Cell>
+                          <Popover
+                            aria-labelledby="default-popover"
+                            content={
+                              <div className="flex justify-center">
+                                <ListGroup className="w-48">
+                                  <ListGroup.Item
+                                    onClick={() =>
+                                      apiClient.contests
+                                        .remove(contest.id)
+                                        .then(() => refetch())
+                                    }
+                                    icon={HiOutlineTrash}
+                                  >
+                                    Usuń
+                                  </ListGroup.Item>
+                                </ListGroup>
+                              </div>
+                            }
+                          >
+                            <Button outline color="gray">
+                              <HiDotsVertical />
+                            </Button>
+                          </Popover>
+                        </Table.Cell>
+                      </Table.Row>
+                    ))}
+                  </Table.Body>
+                ) : (
+                  <Spinner />
+                )}
+              </Table>
+            </div>
           </div>
         </div>
       </div>

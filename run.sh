e02a6cece3e716f316a0b007f5edc1144a9818b7
Skoppek <skopekv21@gmail.com>
2024-10-12 00:11:04 +0200

    switched to docker compose

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-(cd ./judge0; docker-compose up -d db redis; docker-compose up -d)
-docker-compose up -d --build
-docker-compose rm -v -f
+(cd ./judge0; docker compose up -d db redis; docker compose up -d)
+docker compose up -d --build
+docker compose rm -v -f

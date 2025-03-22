977528132fbac04c181485399bdc5a6aaaa5fd19
Mateusz Skop <skopekv21@gmail.com>
2025-03-22 15:35:46 +0100

    update

--- frontend/src/client/edenClient.ts
+++ frontend/src/client/edenClient.ts
@@ -1,12 +1,12 @@
 import { treaty } from "@elysiajs/eden";
 import type { App } from "../../../backend/src";
 
-// @ts-ignore
+// @ts-expect-error yes
 export default treaty<App>(
   import.meta.env.VITE_API_URL,
   {
     fetch: {
       credentials: "include",
-    },
+    }
   },
 );
